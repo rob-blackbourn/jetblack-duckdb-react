afbcc4d969edf0e1cb64f26d28219c5d3deb65c7
Rob Blackbourn <rob.blackbourn@gmail.com>
2024-03-01 17:11:54 +0000

    Added some doc comments

--- lib/DuckDBContext.tsx
+++ lib/DuckDBContext.tsx
@@ -3,9 +3,13 @@ import { createContext } from 'react'
 import { AsyncDuckDB, InstantiationProgress } from '@duckdb/duckdb-wasm'
 
 export type DuckDBContextProps = {
+  /** The DuckDB database if the database instantiation succeed. */
   db?: AsyncDuckDB
+  /** A boolean to indicate whether the database is being loaded. */
   loading: boolean
+  /** The error if the database instantiation failed. */
   error?: Error | string
+  /** The progress; updated during instantiation. */
   progress?: InstantiationProgress
 }
 
