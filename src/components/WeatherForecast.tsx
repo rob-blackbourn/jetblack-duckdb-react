e35f5c181cb77c8d262187774aa0c2adfd74c6dc
Rob Blackbourn <rob.blackbourn@gmail.com>
2024-03-03 12:47:02 +0000

    Use latest weather forecast code

--- src/components/WeatherForecast.tsx
+++ src/components/WeatherForecast.tsx
@@ -5,61 +5,69 @@ import { useDuckDB } from '../../lib/main'
 import { arrowRowGenerator } from '../utils'
 import weatherData from '../assets/data.json'
 
-export default function WeatherForecast() {
-  const { db, loading, error } = useDuckDB()
-  const [isTableLoaded, setIsTableLoaded] = useState(false)
+interface Observation {
+  time: Date
+  temperature: number
+}
 
-  // Load the data into DuckDB.
+export default function WeatherForecast() {
+  const { db } = useDuckDB()
+  const [isTableLoaded, setIsTableLoaded] = useState(false)
+  const [observations, setObservations] = useState<Observation[]>([])
+
   useEffect(() => {
     if (!db) {
       return
     }
     const asyncFunc = async () => {
-      console.log(weatherData)
-
-      console.log('Save the data as a file in the DuckDB file system.')
-      await db.registerFileText(
-        'observations.json',
-        JSON.stringify(weatherData)
-      )
-
-      console.log(
-        'Load the table from the DuckDB file system into the database.'
-      )
+      const text = JSON.stringify(weatherData)
+      await db.registerFileText('observations.json', text)
       const con = await db.connect()
       await con.insertJSONFromPath('observations.json', {
         name: 'observations'
       })
 
-      console.log('Table loaded')
-      setIsTableLoaded(true)
+      return true
     }
 
-    asyncFunc().catch(console.error)
+    asyncFunc().then(setIsTableLoaded).catch(console.error)
   }, [db])
 
-  // Query the database table.
   useEffect(() => {
     if (!(isTableLoaded && db)) {
       return
     }
 
     const asyncFunc = async () => {
-      console.log('Querying table')
       const con = await db.connect()
-      const table = await con.query('SELECT * FROM observations;')
+      const table = await con.query('SELECT * FROM observations LIMIT 10;')
       const rows = Array.from(
         arrowRowGenerator(table, (key, value) =>
           key === 'time' && typeof value === 'number' ? new Date(value) : value
         )
-      )
-      console.log({ rows })
+      ) as Observation[]
+      return rows
     }
 
-    asyncFunc().catch(console.error)
+    asyncFunc().then(setObservations).catch(console.error)
   }, [isTableLoaded, db])
 
-  console.log({ db, loading, error })
-
-  return <div>Weather Forecast</div>
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Time</th>
+          <th style={{ textAlign: 'right' }}>Temperature</th>
+        </tr>
+      </thead>
+      <tbody>
+        {observations.map(({ time, temperature }) => (
+          <tr key={time.valueOf()}>
+            <td>{time.toISOString()}</td>
+            <td style={{ textAlign: 'right' }}>{temperature.toFixed(2)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
 }
